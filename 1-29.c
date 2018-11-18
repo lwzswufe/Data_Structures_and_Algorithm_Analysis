5be33fba1729b225293b02b601cc20ff6ec308df
lwzswufe <lwzswufe@foxmail.com>
2018-11-18 12:53:20 +0800

    交换排序

---
+++ 1-29.c
@@ -0,0 +1,76 @@
+/*
+10-排序6 Sort with Swap(0, i) （25 分）
+Given any permutation of the numbers {0, 1, 2,..., N−1}, it is easy to 
+sort them in increasing order. But what if Swap(0, *) is the ONLY operation 
+that is allowed to use? For example, to sort {4, 0, 2, 1, 3} we may apply 
+the swap operations in the following way:
+
+Swap(0, 1) => {4, 1, 2, 0, 3}
+Swap(0, 3) => {4, 1, 2, 3, 0}
+Swap(0, 4) => {0, 1, 2, 3, 4}
+Now you are asked to find the minimum number of swaps need to sort the 
+given permutation of the first N nonnegative integers.
+
+Input Specification:
+Each input file contains one test case, which gives a positive N (≤10^5) 
+followed by a permutation sequence of {0, 1, ..., N−1}. All the numbers 
+in a line are separated by a space.
+
+Output Specification:
+For each case, simply print in a line the minimum number of swaps need 
+to sort the given permutation.
+
+Sample Input:
+10
+3 5 7 2 6 4 9 0 8 1
+Sample Output:
+9
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int read_input(int N)
+{   
+    int idx, len, n=0;
+    int arr[N], visit[N];
+    for (int i=0; i<N; i++)
+    {
+        scanf("%d", arr+i);
+        if (arr[i] == i)
+            visit[i] = 1;
+        else
+            visit[i] = 0;
+        if (arr[i] == 0)
+            idx = i;
+    }
+
+    for(int i=0; i<N; i++)
+    {
+        if (visit[i] == 1)
+            continue;
+        len = 1;
+        idx = i;
+        while (visit[idx] == 0)
+        {   
+            //printf("visit %d\n", idx);
+            if (arr[idx] == 0)
+                len -= 2;
+            visit[idx] = 1;
+            idx = arr[idx];
+            len++;
+        }
+        n += len;
+    }
+    return n;
+}
+
+int main(int argc, char const *argv[])
+{
+    int N, n;
+    scanf("%d", &N);
+    n = read_input(N);
+    printf("%d", n);
+    scanf("%d", &N);
+    return 0;
+}
